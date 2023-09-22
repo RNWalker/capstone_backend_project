dc2bc759702e6b14c8abc6bf4ec305bbc0bd5379
sarahoco014 <soconnor2@outlook.com>
2023-09-22 15:14:03 +0100

    completed logic for updateOrderStatus for ongoing and onhold

--- src/main/java/com/capstone/CapstoneProject/services/OrderService.java
+++ src/main/java/com/capstone/CapstoneProject/services/OrderService.java
@@ -1,8 +1,11 @@
 package com.capstone.CapstoneProject.services;
 
 import com.capstone.CapstoneProject.models.Order;
+import com.capstone.CapstoneProject.models.Truck;
 import com.capstone.CapstoneProject.models.enums.Status;
+import com.capstone.CapstoneProject.repositories.EmployeeRepository;
 import com.capstone.CapstoneProject.repositories.OrderRepository;
+import com.capstone.CapstoneProject.repositories.TruckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,12 @@ public class OrderService {
     @Autowired
     OrderRepository orderRepository;
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    TruckRepository truckRepository;
+
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
     }
@@ -23,8 +32,19 @@ public class OrderService {
         return orderRepository.findById(id);
     }
 
-    public void updateOrderStatus(Status status, Long orderId) {
+    public void updateOrderStatus(Status status, Long orderId, Long employeeId, Long truckId) {
         Order orderToUpdate = orderRepository.findById(orderId).get();
         orderToUpdate.setStatus(status);
+        if(status.equals(Status.ONGOING)) {
+            orderToUpdate.setEmployee(employeeRepository.findById(employeeId).get());
+            orderToUpdate.setTruck(truckRepository.findById(truckId).get());
+        }
+        if(status.equals(Status.ONHOLD)) {
+            orderToUpdate.setTruck(null);
+            Truck truckToUpdate = truckRepository.findById(truckId).get();
+            truckToUpdate.removeOrder(orderToUpdate);
+            truckRepository.save(truckToUpdate);
+        }
+        orderRepository.save(orderToUpdate);
     }
 }
