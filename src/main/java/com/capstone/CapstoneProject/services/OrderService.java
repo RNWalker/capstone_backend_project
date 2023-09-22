28a3b8d1b6c2b2cf94f1af18fcafe19a6dfb86f3
sarahoco014 <soconnor2@outlook.com>
2023-09-22 14:51:22 +0100

    populates services

--- src/main/java/com/capstone/CapstoneProject/services/OrderService.java
+++ src/main/java/com/capstone/CapstoneProject/services/OrderService.java
@@ -1,4 +1,30 @@
 package com.capstone.CapstoneProject.services;
 
+import com.capstone.CapstoneProject.models.Order;
+import com.capstone.CapstoneProject.models.enums.Status;
+import com.capstone.CapstoneProject.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public void updateOrderStatus(Status status, Long orderId) {
+        Order orderToUpdate = orderRepository.findById(orderId).get();
+        orderToUpdate.setStatus(status);
+    }
 }
